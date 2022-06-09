a7d14b6b6a369c5016d062d9f74bda973b329703
Toshio Kuratomi <a.badger@gmail.com>
2022-06-08 19:00:19 -0700

    Move common data for tests to its own file.

--- tests/test_savefile.py
+++ tests/test_savefile.py
@@ -1,36 +1,18 @@
 import io
-import json
 
+from . import testdata
 from suds import savefile
-
-good_data = {
-    'board': {
-        'rows': [
-            [0, 0, 7, 0, 0, 0, 0, 0, 6],
-            [0, 6, 0, 8, 0, 0, 2, 0, 5],
-            [0, 0, 8, 0, 6, 9, 3, 0, 0],
-            [7, 0, 6, 0, 3, 8, 1, 0, 0],
-            [4, 8, 9, 0, 1, 0, 7, 6, 3],
-            [0, 0, 3, 9, 7, 0, 5, 0, 8],
-            [0, 0, 5, 6, 8, 0, 4, 0, 0],
-            [8, 0, 4, 0, 0, 7, 0, 5, 0],
-            [6, 0, 0, 0, 0, 0, 9, 0, 0],
-        ]
-    }
-}
-
-good_data_json = json.dumps(good_data)
 
 
 def test_load_save_file_filename(tmpdir):
     tmpsavefile = tmpdir / 'save.json'
     with open(tmpsavefile, 'w', encoding='utf-8') as f:
-        f.write(good_data_json)
+        f.write(testdata.GOOD_DATA_JSON)
 
-    assert savefile.load_save_file(tmpsavefile) == good_data
+    assert savefile.load_save_file(tmpsavefile) == testdata.GOOD_DATA
 
 
 def test_load_save_file_file_obj():
-    file_obj = io.StringIO(good_data_json)
+    file_obj = io.StringIO(testdata.GOOD_DATA_JSON)
 
-    assert savefile.load_save_file(file_obj) == good_data
+    assert savefile.load_save_file(file_obj) == testdata.GOOD_DATA
