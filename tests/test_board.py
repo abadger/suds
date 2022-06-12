777dca521eee9337c71ad36aa33861f59ce778a0
Toshio Kuratomi <a.badger@gmail.com>
2022-06-12 12:46:35 -0700

    Complete the removal of SudokuCell.solved test cases

--- tests/test_board.py
+++ tests/test_board.py
@@ -40,24 +40,6 @@ def input_format_to_view_format(values):
     return _list_to_tuple(_values_to_cells(values))
 
 
-@pytest.fixture
-def sboard(request):
-    new_board = board.SudokuBoard()
-    sudoku_data_marker = request.node.get_closest_marker('sudoku_data')
-
-    if not sudoku_data_marker:
-        return new_board
-
-    board_update_format = {}
-    for row_idx, row in enumerate(sudoku_data_marker.args[0]):
-        for col_idx, cell_value in enumerate(row):
-            if cell_value:
-                board_update_format[(row_idx, col_idx)] = cell_value
-
-    new_board.update(board_update_format)
-    return new_board
-
-
 #
 # SudokuCell tests
 #
